b85f4a6362b69a17040588eb8b9d6fe198b3c8ee
Christian <explosivfibres@hotmail.com>
2024-02-16 14:36:08 +0100

    x

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import AddCard from './pages/AddCard/AddCard';
-import Layout from './components/Layout/Layout';
+import Card from './components/Card/Card';
 
 const App = () => {
   return (
-    <Router>
-      <Layout>
-        <Switch>
-          <Route exact path="/cards" component={Home} />
-          <Route path="/addcard" component={AddCard} />
-          {/* Other routes can go here */}
-        </Switch>
-      </Layout>
-    </Router>
+    <Card/>
   );
 };
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
 import './Card.scss';
+import PropTypes from 'prop-types';
 
 const Card = ({ cardDetails }) => {
-  // Component logic
-  return <div className="card">{/* Card content */}</div>;
+  return (
+    <div className="card">
+      <div>{cardDetails.cardNumber}</div>
+      <div>CARDHOLDER NAME: {cardDetails.cardholderName}</div>
+      <div>VALID THRU: {cardDetails.expiryDate}</div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  cardDetails: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    cardNumber: PropTypes.string.isRequired,
+    expiryDate: PropTypes.string.isRequired,
+    cardholderName: PropTypes.string.isRequired,
+    ccv: PropTypes.number.isRequired,
+  }),
 };
 
 export default Card;
