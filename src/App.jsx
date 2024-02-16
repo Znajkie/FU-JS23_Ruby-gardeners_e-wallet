6f33501e55fc2f73f7ccaff4c2c503b8198735d2
Christian <explosivfibres@hotmail.com>
2024-02-16 12:02:50 +0100

    template

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import AddCard from './pages/AddCard/AddCard';
+import Layout from './components/Layout/Layout';
 
-function App() {
-
+const App = () => {
   return (
-    <>
-    </>
-  )
-}
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path="/cards" component={Home} />
+          <Route path="/addcard" component={AddCard} />
+          {/* Other routes can go here */}
+        </Switch>
+      </Layout>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Card.scss';
+
+const Card = ({ cardDetails }) => {
+  // Component logic
+  return <div className="card">{/* Card content */}</div>;
+};
+
+export default Card;
